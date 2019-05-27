7e0b6f804328792e7c136a257e6d7395218e01aa
tttkkkkk <test_t_55@yahoo.co.jp>
2019-05-27 22:38:49 +0900

    title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
-    base_title = "PG Training"
+    base_title = "PG Words"
     if page_title.empty?
       base_title
     elsif @micropost.present?
